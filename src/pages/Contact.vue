0388dedf0fc6f76d2ba23696a7b2fec55b3f8222
Victor <fisheve@mx.lakeforest.edu>
2022-10-01 01:59:46 -0500

    Update Contact.vue

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -157,7 +157,7 @@
       },
       handleSubmit(e) {
         if (this.validate()) {
-          fetch('https://script.google.com/macros/s/AKfycbyywSpkXGYxpuXFJTrwystF1e7CV88Ti9tyC8HpLARRBpExQEbS9cz23ldqAaD0yyq9tQ/exec', {
+          fetch('https://script.google.com/macros/s/AKfycbz7REr-7xS67znORrouW77NyNkwyLCNEfnEfl9VcLGwFIn-BLAXCY_wsx-4XNoCyldUaw/exec', {
             method: 'POST',
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
             body: this.encode({
